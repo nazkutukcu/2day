44b98c88c56285fdb822100ddf3a7b9917d7309c
nazkutukcu <nazkutukcu@gmail.com>
2023-02-17 09:09:50 +0300

    clear

--- set_array.ts
+++
@@ -1,9 +0,0 @@
-let setArray= new Set();
-setArray.add("ahmet");
-setArray.add("mehmet");
-setArray.add("hasan");
-// setArray.add("ahmet");   //kullanmış olduğumuz key'ler set kullandığımızda unique olmalı
-
-for(let name of setArray){
-    console.log(name);
-}
