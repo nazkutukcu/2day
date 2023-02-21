e9087995996cec4b0c9c008bf9958a1d83b7425e
nazkutukcu <nazkutukcu@gmail.com>
2023-02-21 12:09:39 +0300

    clear2

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -10,21 +10,5 @@ import { Product2 } from './models/product2';
 })
 export class AppComponent {
   title = 'my-app';
-  //one-way binding
-  name:string='angular';
-  product:Product;
-  product2:Product2;
-  person:IPerson;
-  //person2 :IPerson;
-  constructor(){    //Tek const
-    this.product=new Product(1,"kalem",200)
-    this.product2={id:1,name:"kalem 2",price:300}
-
-    this.person={id:1,name:"ahmet",surname:"yıldız",age:20}
-    
-  }
-  change(){      
-    this.name='angular 15';
-    
-    }
+  
 }
