b2e9adf5789bcfc77105a50b2ea2b18593090416
nazkutukcu <nazkutukcu@gmail.com>
2023-02-27 15:49:11 +0300

    16.gün tamamlandı

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { TemplateDrivenComponent } from './forms/template-driven/template-driven.component';
+import { ToWayBindingComponent } from './forms/to-way-binding/to-way-binding.component';
+import { ReactiveFormsComponent } from './forms/reactive-forms/reactive-forms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
    
-    TemplateDrivenComponent
+    TemplateDrivenComponent,
+        ToWayBindingComponent,
+        ReactiveFormsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ app/src/app/forms/to-way-binding/to-way-binding.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-way-binding',
+  templateUrl: './to-way-binding.component.html',
+  styleUrls: ['./to-way-binding.component.css']
+})
+export class ToWayBindingComponent {
+name:string="ahmet";
+
+change(){
+  this.name="mehmet";
+
+}
+}
