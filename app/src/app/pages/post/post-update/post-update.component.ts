3b0f0372a4858bcb7a6e8c50b652ea77eed02056
nazkutukcu <nazkutukcu@gmail.com>
2023-03-01 13:23:44 +0300

    öğleden önce tamamlandı

---
+++ app/src/app/pages/post/post-update/post-update.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-post-update',
+  templateUrl: './post-update.component.html',
+  styleUrls: ['./post-update.component.css']
+})
+export class PostUpdateComponent implements OnInit {
+
+  constructor(private activatedRoute: ActivatedRoute){}
+
+
+  ngOnInit(): void {
+
+   var id= this.activatedRoute.snapshot.queryParams['id'];
+   console.log(id);
+
+   this.activatedRoute.queryParamMap.subscribe(x=>{
+    console.log("queryParamMap",x);
+    console.log("queryParamMap:" + x.get('id'));
+   });
+
+   this.activatedRoute.queryParams.subscribe(x=>{
+    console.log("queryParams",x)
+   });
+
+
+
+  }
+
+}
