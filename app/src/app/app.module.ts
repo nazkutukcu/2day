3793c904e9d8ef5560e05a2ad20c9859e15a8470
nazkutukcu <nazkutukcu@gmail.com>
2023-03-02 11:45:41 +0300

    clear

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,18 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserModule } from './user/user.module';
-import { HomeComponent } from './pages/home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UserModule //eager loading
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
