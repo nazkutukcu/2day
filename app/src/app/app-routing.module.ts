2ffbbac1d420917e1368e4ee352da2e3928c791b
nazkutukcu <nazkutukcu@gmail.com>
2023-03-02 13:28:31 +0300

    çalışma tamamlandı

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,20 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { UserCreateComponent } from './user/user-create/user-create.component';
-import { UserListComponent } from './user/user-list/user-list.component';
+import { HomeComponent } from './main/home/home.component';
+import { AboutComponent } from './main/about/about.component';
+import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
+import { UserListComponent } from './admin/user-list/user-list.component';
+import { AuthGuard, canActivateNewFormat } from './auth.guard';
+import { LoginComponent } from './main/login/login.component';
+import { ChildGuard } from './child.guard';
+import { EditGuard, EditGuardNewType } from './edit.guard';
+import { CanOrderLoad } from './canload';
 
 const routes: Routes = [
 
-  {path:'home',component:HomeComponent},
-  {path:'',component:HomeComponent},
-  {path:'user-list',component:UserListComponent},
-  {path:'user-create',component:UserCreateComponent},
-  {path:'post',
-  loadChildren:() => import('./post/post.module').then((m)=>m.
-  PostModule),
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+  {
+    path: 'admin', canActivate: [AuthGuard, canActivateNewFormat], canActivateChild: [ChildGuard], children: [
+      { path: '', component: AdminHomeComponent },
+      { path: 'home', component: AdminHomeComponent },
 
+      { path: 'user-list', component: UserListComponent, canDeactivate: [EditGuardNewType] }
+
+
+    ]
+  },
+
+
+  {
+    path: "lazy-order", canLoad:[CanOrderLoad], loadChildren: () => import('./order/order.module').then(m => m.OrderModule)
   }
+
 
 
 ];
