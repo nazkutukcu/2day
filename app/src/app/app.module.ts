824cda00c885216f19305885a331f3d62e380b75
nazkutukcu <nazkutukcu@gmail.com>
2023-03-06 15:26:37 +0300

    tamamlandı

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './directieves/highlight.directive';
+import { CounterDirective } from './directieves/counter.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HighlightDirective,
+    CounterDirective,
 
   ],
   imports: [
